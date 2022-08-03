301d16ce7112e97cfc05b5921be14818cdf18cd6
soeun-nho <so84392091@gmail.com>
2022-08-03 11:46:51 +0900

    하위 4개 정보 추가

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -6,14 +6,14 @@ var map = new naver.maps.Map('map', {
  
 // 가게 정보 배열
 var place = {
-    1: { "title": '제나키친', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6034, 127.04169) },
+    1: { "title": '제나키친', "latlng": new naver.maps.LatLng(37.6034, 127.04169) },
     2: { "title":"조아버거", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6039015, 127.0408758) },
     3: { "title": '송송식탁', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6038977, 127.0427576) },
     4: { "title": '스시빈', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.60385, 127.0433) },
-    5: { "title": '백소정', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6028850, 127.0412987)},
-    6: { "title":"서브웨이", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.60384, 127.04272) },
-    7: { "title": '핏짜피자', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6037559, 127.0420138) },
-    8: { "title": '샐러디', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", "latlng": new naver.maps.LatLng(37.6041401,127.0428911) }
+    5: { "title": '백소정',  closeD:"null", openH:"11", openM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"0" , "latlng": new naver.maps.LatLng(37.6028850, 127.0412987)},
+    6: { "title":"서브웨이",  closeD:"null", openH:"8", openM:"0", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"22", closedM:"0" ,"latlng": new naver.maps.LatLng(37.60384, 127.04272) },
+    7: { "title": '핏짜피자', closeD: "null", openH:"11", openM:"0", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"30" , "latlng": new naver.maps.LatLng(37.6037559, 127.0420138) },
+    8: { "title": '샐러디',  closeD:"null", openH:"8", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"21", closedM:"0" , "latlng": new naver.maps.LatLng(37.6041401,127.0428911) }
 }
   
 for (var i = 1; i < 9; i++) {
