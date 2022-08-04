83adb9bea3724bd63d20dd2fe1d8fe6ef7123853
soeun-nho <so84392091@gmail.com>
2022-08-04 17:00:15 +0900

    스크립트 도희 언니 받기

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -8,14 +8,18 @@ var infoWindows = new Array(); // 정보창을 담는 배열
 var positions = new Array();  // 지역을 담는 배열 ( 지역명/위도경도 )
 
 positions.push(
-    { "title": '제나키친', foodtype: "한식" , closeD: "Sat", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
+    //{ "title": '제나키친', foodtype: "한식" , closeD: "Sat", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
     { "title":"조아버거",  foodtype: "햄버거", closeD: "Sun", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"NUll", breakOM:"NUll", breakCH:"NUll", breakCM:"NUll", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
     { "title": '송송식탁', foodtype: "한식",  closeD: "Sun", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
     { "title": '스시빈',  foodtype: "초밥/롤",closeD: "Sun", openH:"11", openM:"30", closeH:"22", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
     { "title": '백소정',   foodtype: "일식당",closeD:"null", openH:"11", openM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00" ,closeH:"21", closeM:"00" , latlng: new naver.maps.LatLng(37.6028850, 127.0412987)},
     { "title":"서브웨이",  foodtype: "샌드위치", closeD:"null", openH:"08", openM:"00", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"22", closeM:"00" ,latlng: new naver.maps.LatLng(37.60384, 127.04272) },
-    { "title": '핏짜피자',  foodtype: "피자",closeD: "null", openH:"11", openM:"00", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"00" ,closeH:"21", closeM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
-    { "title": '샐러디',   foodtype: "샐러드", closeD:"null", openH:"08", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"21", closeM:"00" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
+    { "title":"밥은화",  foodtype: "한식", closeD:"Sat", openH:"11", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"20", closeM:"30" ,latlng: new naver.maps.LatLng(37.6057359, 127.044453) },
+    { "title":"연이네 과자점",  foodtype: "카페, 디저트", closeD:"Sat", openH:"11", openM:"00", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"20", closeM:"00" ,latlng: new naver.maps.LatLng(37.6038866, 127.0415479) }
+    //토,일은 브에리크 타임이 없음
+    //{ "title": '핏짜피자',  foodtype: "피자",closeD: "null", openH:"11", openM:"00", breakOH:"15,null", breakOM:"30,null", breakCH:"17,null", breakCM:"00,null" ,closeH:"21", closeM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
+    //토, 일 영업 시간 다름 (10:00-20:00)
+    //{ "title": '샐러디',   foodtype: "샐러드", closeD:"null", openH:"08", openH2:"10", openM:"30",openM2:"00",  breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"21", closeM:"00" ,closeH2:"20", closeM2:"00" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
 );
 	
 for (var i = 0; i < positions.length; i++) {
@@ -30,9 +34,9 @@ for (var i = 0; i < positions.length; i++) {
         /* 정보창 */
         var infoWindow = new naver.maps.InfoWindow({
             content: '<div style="width:200px;text-align:center;padding:10px;"><strong>' + positions[i].title + '</strong><br/>'
-            +positions[i].foodtype + '<br>' 
+            +'<b>'+positions[i].foodtype+'<b>'+ '<br>' 
             +positions[i].openH + ':'+ positions[i].openM + '~'+positions[i].closeH +":" +positions[i].closeM+'</div>'
-        }); // 클릭했을 때 띄워줄 정보 HTML 작성
+           }); // 클릭했을 때 띄워줄 정보 HTML 작성
         
     markers.push(marker); // 생성한 마커를 배열에 담는다.
     infoWindows.push(infoWindow); // 생성한 정보창을 배열에 담는다.
@@ -56,3 +60,10 @@ for (var i=0; i<markers.length; i++) {
     console.log(markers[i] , getClickHandler(i));
     naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i)); // 클릭한 마커 핸들러
 }
+
+
+              var d = document.getElementById("d");
+              var day = d.options[d.selectedIndex].value;
+            //   document.getElementById("nav").innerHTML = day;
+     document.write(day);
+
