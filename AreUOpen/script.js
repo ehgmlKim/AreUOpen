26f4d91b852ff56a08c66ada8dd563cea0aa9d9d
soeun-nho <so84392091@gmail.com>
2022-08-04 12:28:32 +0900

    정보창 수정 음식종류 업로드

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -1,6 +1,6 @@
 var map = new naver.maps.Map('map', {
     center: new naver.maps.LatLng(37.6034, 127.04169), //지도 시작 지점
-    zoom: 15
+    zoom: 17
 });
 
 var markers = new Array(); // 마커 정보를 담는 배열
@@ -8,14 +8,14 @@ var infoWindows = new Array(); // 정보창을 담는 배열
 var positions = new Array();  // 지역을 담는 배열 ( 지역명/위도경도 )
 
 positions.push(
-    { "title": '제나키친', closeD: "Sat", openH:"11", openM:"0", closeH:"20", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
-    { "title":"조아버거", closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"NUll", breakOM:"NUll", breakCH:"NUll", breakCM:"NUll", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
-    { "title": '송송식탁', closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
-    { "title": '스시빈', closeD: "Sun", openH:"11", openM:"30", closeH:"22", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
-    { "title": '백소정',  closeD:"null", openH:"11", openM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"0" , latlng: new naver.maps.LatLng(37.6028850, 127.0412987)},
-    { "title":"서브웨이",  closeD:"null", openH:"8", openM:"0", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"22", closedM:"0" ,latlng: new naver.maps.LatLng(37.60384, 127.04272) },
-    { "title": '핏짜피자', closeD: "null", openH:"11", openM:"0", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
-    { "title": '샐러디',  closeD:"null", openH:"8", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"21", closedM:"0" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
+    { "title": '제나키친', foodtype: "한식" , closeD: "Sat", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
+    { "title":"조아버거",  foodtype: "햄버거", closeD: "Sun", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"NUll", breakOM:"NUll", breakCH:"NUll", breakCM:"NUll", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
+    { "title": '송송식탁', foodtype: "한식",  closeD: "Sun", openH:"11", openM:"00", closeH:"20", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
+    { "title": '스시빈',  foodtype: "초밥/롤",closeD: "Sun", openH:"11", openM:"30", closeH:"22", closeM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
+    { "title": '백소정',   foodtype: "일식당",closeD:"null", openH:"11", openM:"00", breakOH:"15", breakOM:"00", breakCH:"17", breakCM:"00" ,closeH:"21", closeM:"00" , latlng: new naver.maps.LatLng(37.6028850, 127.0412987)},
+    { "title":"서브웨이",  foodtype: "샌드위치", closeD:"null", openH:"08", openM:"00", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"22", closeM:"00" ,latlng: new naver.maps.LatLng(37.60384, 127.04272) },
+    { "title": '핏짜피자',  foodtype: "피자",closeD: "null", openH:"11", openM:"00", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"00" ,closeH:"21", closeM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
+    { "title": '샐러디',   foodtype: "샐러드", closeD:"null", openH:"08", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"21", closeM:"00" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
 );
 	
 for (var i = 0; i < positions.length; i++) {
@@ -27,11 +27,13 @@ for (var i = 0; i < positions.length; i++) {
         position: positions[i].latlng // 지역구의 위도 경도 넣기 
     });
     
-    /* 정보창 */
-    var infoWindow = new naver.maps.InfoWindow({
-        content: '<div style="width:200px;text-align:center;padding:10px;"><b>' + positions[i].title + '</b><br> - 네이버 지도 - </div>'
-    }); // 클릭했을 때 띄워줄 정보 HTML 작성
-    
+        /* 정보창 */
+        var infoWindow = new naver.maps.InfoWindow({
+            content: '<div style="width:200px;text-align:center;padding:10px;"><strong>' + positions[i].title + '</strong><br/>'
+            +positions[i].foodtype + '<br>' 
+            +positions[i].openH + ':'+ positions[i].openM + '~'+positions[i].closeH +":" +positions[i].closeM+'</div>'
+        }); // 클릭했을 때 띄워줄 정보 HTML 작성
+        
     markers.push(marker); // 생성한 마커를 배열에 담는다.
     infoWindows.push(infoWindow); // 생성한 정보창을 배열에 담는다.
 }	
