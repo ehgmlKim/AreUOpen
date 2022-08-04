5b96260b1171e09c26c06c48f47e2669a7416564
ehgmlKim <dohee930@gmail.com>
2022-08-04 11:53:54 +0900

    배열에 다 넣고 정보창 띄우는 기능 완성

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -1,29 +1,56 @@
-//지도
 var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.6034, 127.04169),
-    zoom: 17
-  });
- 
-// 가게 정보 배열
-var place = {
-    1: { "title": '제나키친', closeD: "Sat", openH:"11", openM:"0", closeH:"20", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
-    2: { "title":"조아버거", closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"NUll", breakOM:"NUll", breakCH:"NUll", breakCM:"NUll", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
-    3: { "title": '송송식탁', closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
-    4: { "title": '스시빈', closeD: "Sun", openH:"11", openM:"30", closeH:"22", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
-    5: { "title": '백소정',  closeD:"null", openH:"11", openM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"0" , "latlng": new naver.maps.LatLng(37.6028850, 127.0412987)},
-    6: { "title":"서브웨이",  closeD:"null", openH:"8", openM:"0", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"22", closedM:"0" ,"latlng": new naver.maps.LatLng(37.60384, 127.04272) },
-    7: { "title": '핏짜피자', closeD: "null", openH:"11", openM:"0", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"30" , "latlng": new naver.maps.LatLng(37.6037559, 127.0420138) },
-    8: { "title": '샐러디',  closeD:"null", openH:"8", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"21", closedM:"0" , "latlng": new naver.maps.LatLng(37.6041401,127.0428911) }
-}
-  
-for (var i = 1; i < 9; i++) {
-    // 마커를 생성합니다
+    center: new naver.maps.LatLng(37.6034, 127.04169), //지도 시작 지점
+    zoom: 15
+});
+
+var markers = new Array(); // 마커 정보를 담는 배열
+var infoWindows = new Array(); // 정보창을 담는 배열
+var positions = new Array();  // 지역을 담는 배열 ( 지역명/위도경도 )
+
+positions.push(
+    { "title": '제나키친', closeD: "Sat", openH:"11", openM:"0", closeH:"20", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"30", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
+    { "title":"조아버거", closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"NUll", breakOM:"NUll", breakCH:"NUll", breakCM:"NUll", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
+    { "title": '송송식탁', closeD: "Sun", openH:"11", openM:"0", colseH:"20", colseM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
+    { "title": '스시빈', closeD: "Sun", openH:"11", openM:"30", closeH:"22", closeM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
+    { "title": '백소정',  closeD:"null", openH:"11", openM:"0", breakOH:"15", breakOM:"0", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"0" , latlng: new naver.maps.LatLng(37.6028850, 127.0412987)},
+    { "title":"서브웨이",  closeD:"null", openH:"8", openM:"0", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"22", closedM:"0" ,latlng: new naver.maps.LatLng(37.60384, 127.04272) },
+    { "title": '핏짜피자', closeD: "null", openH:"11", openM:"0", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"0" ,closedH:"21", closedM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
+    { "title": '샐러디',  closeD:"null", openH:"8", openM:"30", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closedH:"21", closedM:"0" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
+);
+	
+for (var i = 0; i < positions.length; i++) {
+    // 지역을 담은 배열의 길이만큼 for문으로 마커와 정보창을 채워주자 !
+
     var marker = new naver.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: place[i]["latlng"], // 마커를 표시할 위치
-        title : place[i]["title"] // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다 -> 이것처럼 팝업기능 넣을 수 있을듯?
+        map: map,
+        title: positions[i].title, // 지역구 이름 
+        position: positions[i].latlng // 지역구의 위도 경도 넣기 
     });
+    
+    /* 정보창 */
+    var infoWindow = new naver.maps.InfoWindow({
+        content: '<div style="width:200px;text-align:center;padding:10px;"><b>' + positions[i].title + '</b><br> - 네이버 지도 - </div>'
+    }); // 클릭했을 때 띄워줄 정보 HTML 작성
+    
+    markers.push(marker); // 생성한 마커를 배열에 담는다.
+    infoWindows.push(infoWindow); // 생성한 정보창을 배열에 담는다.
+}	
+	
+function getClickHandler(seq) {
+		
+    return function(e) {  // 마커를 클릭하는 부분
+        var marker = markers[seq], // 클릭한 마커의 시퀀스로 찾는다.
+            infoWindow = infoWindows[seq]; // 클릭한 마커의 시퀀스로 찾는다
+
+        if (infoWindow.getMap()) {
+            infoWindow.close();
+        } else {
+            infoWindow.open(map, marker); // 표출
+        }
+    }
 }
 
-
-
+for (var i=0; i<markers.length; i++) {
+    console.log(markers[i] , getClickHandler(i));
+    naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i)); // 클릭한 마커 핸들러
+}
