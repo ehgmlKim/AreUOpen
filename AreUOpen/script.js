4491f6d3fad112c211fe8f6f3089e880900db879
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2022-11-13 16:00:49 +0900

    modify script.js
    1. 마커 이미지로 변경
    2. 영업시간에 따라 이미지 색상 변경되도록 수정

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -1,9 +1,11 @@
-var map = new naver.maps.Map('map', { //37.6034, 127.04169
-    center: new naver.maps.LatLng(37.6034, 127.04169), //지도 시작 지점
-    zoom: 17
-});
 var markers = new Array(); // 마커 정보를 담는 배열
 var infoWindows = new Array(); // 정보창을 담는 배열
+
+var marker_icon_default_content = '<img src="./img/gray_icon.png" style="width:30px">';
+var marker_icon_open_content = '<img src="./img/blue_icon.png" style="width:30px">';
+var marker_icon_break_content = '<img src="./img/orange_icon.png" style="width:30px">';
+var marker_icon_close_content = '<img src="./img/red_icon.png" style="width:30px">';   //이름 바꿔야함 선택된 애들 아이콘 색도 바꿔야함
+
 var positions = new Array();  // 지역을 담는 배열 ( 지역명/위도경도 )
 
 positions.push(
@@ -18,15 +20,32 @@ positions.push(
 //    { "title": '핏짜피자',  foodtype: "피자",closeD: "null", openH:"11", openM:"00", breakOH:"15", breakOM:"30", breakCH:"17", breakCM:"00" ,closeH:"21", closeM:"30" , latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
 //    { "title": '샐러디',   foodtype: "샐러드", closeD:"null", openH:"08", openM:"30", openH2:"10", openM2:"0", breakOH:"null", breakOM:"null", breakCH:"null" ,breakCM:"null" ,closeH:"21", closeM:"00", closeH2:"2", closeM:"00" , latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
 );
-	
+
+var selectedMarker = null;
+
+//지도 생성
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = { 
+        center: new naver.maps.LatLng(37.6034, 127.04169), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+var map = new naver.maps.Map(mapContainer, mapOption);
+
 for (var i = 0; i < positions.length; i++) {
     // 지역을 담은 배열의 길이만큼 for문으로 마커와 정보창을 채워주자 !
-
-    var marker = new naver.maps.Marker({
+    var markerOptions = {
         map: map,
         title: positions[i].title, // 지역구 이름 
-        position: positions[i].latlng // 지역구의 위도 경도 넣기 
-    });
+        position: positions[i].latlng, // 지역구의 위도 경도 넣기 
+        icon: {
+            content: marker_icon_default_content,
+            size: new naver.maps.Size(22, 35),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(11, 35),
+            scaledSize : new naver.maps.Size(22, 35)
+        }
+    };
+    var marker = new naver.maps.Marker(markerOptions);
     
     /* 정보창 */
     var infoWindow = new naver.maps.InfoWindow({
@@ -41,7 +60,7 @@ for (var i = 0; i < positions.length; i++) {
     markers.push(marker); // 생성한 마커를 배열에 담는다.
     infoWindows.push(infoWindow); // 생성한 정보창을 배열에 담는다.
 }	
-	
+
 function getClickHandler(seq) {
 		
     return function(e) {  // 마커를 클릭하는 부분
@@ -50,15 +69,15 @@ function getClickHandler(seq) {
         
         if (infoWindow.getMap()) {
             infoWindow.close();
-            showMarkers();
+            changeMarker(marker, null);
         } else {
             infoWindow.open(map, marker); // 표출
             for(var i=0; i<markers.length;i++){ //클릭한 마커 제외 다 없애기
                 if(seq != i) {
-                    markers[i].setMap(null);
+                    changeMarker(markers[i],marker);
                 }
             }
-
+            
         }
         
     }
@@ -67,75 +86,133 @@ function getClickHandler(seq) {
 for (var i=0; i<markers.length; i++) {
     naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i)); // 클릭한 마커 핸들러
 }
-// for (var i=0; i<markers.length; i++) {
-//     // console.log(markers[i] , getClickHandler(i));
-//     naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i){
-//      여기에 함수 기능을 쓰고 싶은데 조건문을 써야해서 여기에 못 씀 그래서 위아래 합쳐서 기능 구현함
-//     }); // 클릭한 마커 핸들러
-// }
-// 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
-function setMarkers(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-    }            
+
+function changeMarker(prev_marker, marker) {
+    if (prev_marker != undefined) {
+        prev_marker.setIcon({
+            content : marker_icon_default_content,
+            size: new naver.maps.Size(22, 35),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(11, 35),
+            scaledSize : new naver.maps.Size(22, 35)
+        })
+    }
+    marker.setIcon({
+        content : marker_icon_close_content,
+        size: new naver.maps.Size(22, 35),
+        origin: new naver.maps.Point(0, 0),
+        anchor: new naver.maps.Point(11, 35),
+        scaledSize : new naver.maps.Size(22, 35)
+    });
 }
 
-// "마커 보이기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에 표시하는 함수입니다
-function showMarkers() {
-    setMarkers(map)    
-}
-
-// "마커 감추기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에서 삭제하는 함수입니다
-function hideMarkers() {
-    setMarkers(null);    
-}
+var opens = new Array(); //오픈한 식당
+var closes = new Array(); //닫은 식당
+var breaks = new Array(); //현재 브레이크인 식당
 
 //오픈한 가게만 보이게 하기
 function ChangeValue(){
-   var day = document.getElementById('day');
-   var hour = document.getElementById('hour');
-   var min = document.getElementById('min');
-   var selectedD = day.options[day.selectedIndex].value;
-   var selectedH = parseInt(hour.options[hour.selectedIndex].value);
-   var selectedM = parseInt(min.options[min.selectedIndex].value);
-   for(var i=0;i<positions.length;i++) {
-        var openH = parseInt(positions[i].openH);
-        var openM = parseInt(positions[i].openM);
-        var closeH = parseInt(positions[i].closeH);
-        var closeM = parseInt(positions[i].closeM);
-        var breakOH = parseInt(positions[i].breakOH);
-        var breakOM = parseInt(positions[i].breakOM);
-        var breakCH = parseInt(positions[i].breakCH);
-        var breakCM = parseInt(positions[i].breakCM);
-        if (selectedD == positions[i].closeD) {
-            //휴무일이랑 같으면
-            markers[i].setMap(null); //안보이게
-        } 
-        //휴무일 아닌 애들
-        else if( selectedH < openH) { 
-            //오픈시간보다 작으면
-            markers[i].setMap(null); //안보이게
-        } else if( selectedH == openH && selectedM < openM ) { 
-            //오픈시간보다 같은데 오픈 분보다 작으면
-            markers[i].setMap(null); //안보이게
-        } else if( selectedH > breakOH && selectedH < breakCH) {
-            //브레이크 시작보다 크고 브레이크 끝보다 작으면
-            markers[i].setMap(null);
-        } else if( selectedH == breakOH && selectedM >= breakOM) {
-            //브레이크 시작이랑 같은데 분이 크면
-            markers[i].setMap(null);
-        } else if( selectedH == breakCH && selectedM < breakCM) {
-            //브레이크 끝이랑 같은데 분이 작으면
-            markers[i].setMap(null);
-        } else if( selectedH > closeH) {
-            //끝나는 시간보다 크면
-            markers[i].setMap(null);  //안보이게
-        } else if( selectedH == closeH && selectedM >= closeM) {
-            //끝나는 시간보다 작거나 같은데 끝나는 분보다 같거나 크면
-            markers[i].setMap(null);  //안보이게
-        } else{ //다 아니면 보이게
-            markers[i].setMap(map);
-        }
-   }
-}
+    var day = document.getElementById('day');
+    var hour = document.getElementById('hour');
+    var min = document.getElementById('min');
+    var selectedD = day.options[day.selectedIndex].value;
+    var selectedH = parseInt(hour.options[hour.selectedIndex].value);
+    var selectedM = parseInt(min.options[min.selectedIndex].value);
+    for(var i=0;i<positions.length;i++) {
+         var openH = parseInt(positions[i].openH);
+         var openM = parseInt(positions[i].openM);
+         var closeH = parseInt(positions[i].closeH);
+         var closeM = parseInt(positions[i].closeM);
+         var breakOH = parseInt(positions[i].breakOH);
+         var breakOM = parseInt(positions[i].breakOM);
+         var breakCH = parseInt(positions[i].breakCH);
+         var breakCM = parseInt(positions[i].breakCM);
+         if (selectedD == positions[i].closeD) {
+             //휴무일이랑 같으면
+             markers[i].setIcon({
+                content : marker_icon_close_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } 
+         //휴무일 아닌 애들
+         else if( selectedH < openH) { 
+             //오픈시간보다 작으면
+             markers[i].setIcon({
+                content : marker_icon_default_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH == openH && selectedM < openM ) { 
+             //오픈시간보다 같은데 오픈 분보다 작으면
+             markers[i].setIcon({
+                content : marker_icon_default_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH > breakOH && selectedH < breakCH) {
+             //브레이크 시작보다 크고 브레이크 끝보다 작으면
+             markers[i].setIcon({
+                content : marker_icon_break_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH == breakOH && selectedM >= breakOM) {
+             //브레이크 시작이랑 같은데 분이 크면
+             markers[i].setIcon({
+                content : marker_icon_break_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH == breakCH && selectedM < breakCM) {
+             //브레이크 끝이랑 같은데 분이 작으면
+             markers[i].setIcon({
+                content : marker_icon_break_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH > closeH) {
+             //끝나는 시간보다 크면
+             markers[i].setIcon({
+                content : marker_icon_close_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else if( selectedH == closeH && selectedM >= closeM) {
+             //끝나는 시간보다 작거나 같은데 끝나는 분보다 같거나 크면
+             markers[i].setIcon({
+                content : marker_icon_close_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         } else{ //다 아니면 보이게
+             markers[i].setIcon({
+                content : marker_icon_open_content,
+                size: new naver.maps.Size(22, 35),
+                origin: new naver.maps.Point(0, 0),
+                anchor: new naver.maps.Point(11, 35),
+                scaledSize : new naver.maps.Size(22, 35)
+            })
+         }
+    }
+    
+    
+ }
 
+ 
